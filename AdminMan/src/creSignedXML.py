4dfbce5b549f848badf8d3e7e0dfd5652bd93c84
rhoerbe <rainer@hoerbe.at>
2015-08-12 14:56:06 +0200

    TestWithSig passes

--- AdminMan/src/creSignedXML.py
+++ AdminMan/src/creSignedXML.py
@@ -6,10 +6,11 @@ import re
 __author__ = 'r2h2'
 
 
-def creSignedXML(data):
+def creSignedXML(data, verbose=False):
     ''' compress, b64-encode and sign-envelop the data and return it '''
 
     dataPacked = base64.b64encode(bz2.compress(data))
+    if verbose: print('packed data:\n%s\n' % dataPacked)
 
     sigRequ = '''\
 <?xml version="1.0" encoding="UTF-8"?>
@@ -28,15 +29,21 @@ def creSignedXML(data):
   </sl:DataObjectInfo>
 </sl:CreateXMLSignatureRequest> ''' % dataPacked
 
-    r = requests.post('http://localhost:3495/http-security-layer-request',
-                      data={'XMLRequest': sigRequ})
-    # strip xml root element (CreateXMLSignatureResponse), making disg:Signature the new root
+    try:
+        r = requests.post('http://localhost:3495/http-security-layer-request',
+                          data={'XMLRequest': sigRequ})
+    except requests.exceptions.ConnectionError, e:
+        print("Cannot connect to security layer (MOCCA) to create a signature " + e.strerror)
+        raise
+    assert r.text.find('sl:ErrorResponse') < 0, "Security Layer responed with error message."
 
-
+    # Strip xml root element (CreateXMLSignatureResponse), making disg:Signature the new root:
+    # (keeping namespace prefixes - otherwise the signature would break. Therefore not using etree.)
+    if verbose: print('security layer create signature response:\n%s\n' % r.text)
     r1 = re.sub(r'<sl:CreateXMLSignatureResponse [^>]*>', '', r.text)
     r2 = re.sub(r'</sl:CreateXMLSignatureResponse>', '', r1)
     return r2
 
 
 if __name__ == '__main__':
-    print(creSigRequ('Testdata'))
+    print(creSignedXML('Testdata'))
