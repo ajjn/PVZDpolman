1caefac9e6b9f556c743eec900ee3dbfb8f84ba9
rhoerbe <rainer@hoerbe.at>
2015-09-12 17:39:55 +0200

    git celanup

---
+++ ValidateXSD/src/at/hoerbe/RunVerifyXSD.java
@@ -0,0 +1,30 @@
+package at.hoerbe;
+
+
+import at.wien.ma14.pvzd.validatexsd.XmlValidator;
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+
+public class RunVerifyXSD {
+    final File log4jprop = new File("VerifySigAPI/conf/log4j.properties");
+    final File moaspprop = new File("/opt/java/moa-id-auth-2.2.1/conf/moa-spss/MOASPSSConfiguration.xml");
+
+    public void testVerifyGood() throws Exception {
+        ClassLoader cl = ClassLoader.getSystemClassLoader();
+        URL[] urls = ((URLClassLoader)cl).getURLs();
+        for(URL url: urls){
+            System.out.println(url.getFile());
+        }
+
+        final String samlmdFileOK = "ValidateXSD/testdata/idp5_valid.xml";
+        XmlValidator validator = new XmlValidator("ValidateXSD/SAML_MD_Schema");
+        validator.validateSchema(samlmdFileOK);    }
+
+    public static void main(String[] argv) throws Exception {
+        RunVerifyXSD r = new RunVerifyXSD();
+        r.testVerifyGood();
+    }
+
+}
