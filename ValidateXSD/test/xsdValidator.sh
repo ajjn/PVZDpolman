8e80af76d7aeaa7d0b717bdc089a27866f477338
rhoerbe <rainer@hoerbe.at>
2015-12-08 12:35:34 +0100

    renamed

--- ValidateXSD/test/xsdValidator.sh
+++ ValidateXSD/test/xsdValidator.sh
@@ -4,7 +4,8 @@ export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0.jdk/Contents/Home
 export PROJ_HOME=/Users/admin/devl/java/rhoerbe/PVZD
 export CLASSPATH=$PROJ_HOME/bin/test/ValidateXSD:$PROJ_HOME/bin/production/ValidateXSD:$PROJ_HOME/lib/unittests/junit-4.11.jar
 
-$JAVA_HOME/bin/java at.wien.ma14.pvzd.validatexsd.unittest.RunValidateXSDcli \
-    $PROJ_HOME/ValidateXSD/testdata/gondorMagwienGvAt_ed_invalid_xsd
+$JAVA_HOME/bin/java at.wien.ma14.pvzd.validatexsd.cli.XSDValidatorCLI $@
 
+# argv[0] values
 #    $PROJ_HOME/ValidateXSD/testdata/idp5_valid.xml
+#    $PROJ_HOME/ValidateXSD/testdata/gondorMagwienGvAt_ed_invalid_xsd
