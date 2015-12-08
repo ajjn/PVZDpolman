216a23390afd7215c4cab107517bfeea014041e8
rhoerbe <rainer@hoerbe.at>
2015-12-08 12:54:20 +0100

    refactored xsd validation

--- PolicyManager/src/SAMLEntityDescriptor.py
+++ PolicyManager/src/SAMLEntityDescriptor.py
@@ -3,13 +3,15 @@ from jnius import autoclass
 
 __author__ = 'r2h2'
 
+
 class SAMLEntityDescriptor:
     def __init__(self, filename_abs, projdir_abs):
         self.filename_abs = filename_abs
         self.projdir_abs = projdir_abs
 
     def validateXSD(self):
-        XmlValidator = autoclass('at.wien.ma14.pvzd.validatexsd.XmlValidator')
-        validator = XmlValidator(os.path.join(self.projdir_abs, 'ValidateXSD/SAML_MD_Schema'), False)
-        validator.validateSchema(self.filename_abs)
+        xsdValidator = autoclass('at.wien.ma14.pvzd.validatexsd.XSDValidator')
+        validator = xsdValidator(os.path.join(self.projdir_abs, 'ValidateXSD/SAML_MD_Schema'), False)
+        return validator.validateSchema(self.filename_abs)
+
 
