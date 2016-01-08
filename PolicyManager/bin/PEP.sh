e5e1055d26f81e30409c1b2fc55347c91b0b4e15
r2h2 <rainer@hoerbe.at>
2016-01-08 14:44:38 +0100

    extended setenv.sh for MA14 test system

--- PolicyManager/bin/PEP.sh
+++ PolicyManager/bin/PEP.sh
@@ -2,8 +2,7 @@
 
 source ./setEnv.sh
 
-~/.virtualenvs/pvzd34/bin/python \
-    $PROJ_HOME/PolicyManager/src/PEP.py --verbose \
+$py3    $PROJ_HOME/PolicyManager/src/PEP.py --verbose \
     --aods $aodsfile \
     --pubreq $repo_dir \
     --trustedcerts $trustedcerts
