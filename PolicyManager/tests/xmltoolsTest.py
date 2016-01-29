e16ef50dd74083cd8bb6e135298b1b60d3385f97
r2h2 <rainer@hoerbe.at>
2016-01-29 12:38:50 +0100

    add signature test to xmltoolsTest

--- PolicyManager/tests/xmltoolsTest.py
+++ PolicyManager/tests/xmltoolsTest.py
@@ -1,13 +1,16 @@
 import os.path
 from jnius import autoclass
 from constants import PROJDIR_ABS
+#from xmlsigverifyer import * # XmlSigVerifyer
 
 __author__ = 'r2h2'
 
 
 class TestXmlTools:
     """
+    Used to test java interface and call libraries
     Validates XML schema using Xerces/Java
+    Validates XML signature using MOA-ID
     """
     def __init__(self, xsd_schema_dir):
         self.saml_xsd_dir = os.path.join(PROJDIR_ABS, xsd_schema_dir)
@@ -20,8 +23,23 @@ class TestXmlTools:
     def validate_schematron(self):
         pass  # TODO: implement
 
+
+    def verify_xmlsig(self, xml_file_name):
+            PvzdVerfiySig = autoclass('at.wien.ma14.pvzd.verifysigapi.PvzdVerifySig')
+            verifier = PvzdVerfiySig(
+                os.path.join(PROJDIR_ABS, 'conf/moa-spss/MOASPSSConfiguration.xml'),
+                os.path.join(PROJDIR_ABS, 'conf/log4j.properties'),
+                xml_file_name)
+            response  = verifier.verify()
+            if response.pvzdCode != 'OK':
+                raise ValidationError("Signature verification failed, code=" +
+                                      response.pvzdCode + "; " + response.pvzdMessage)
+
+
 if __name__ == '__main__':
     schema_dir_abs = os.path.join(PROJDIR_ABS, 'lib/SAML_MD_Schema')
-    saml_schema_validator = TestXmlTools(schema_dir_abs)
-    saml_schema_validator.validate_xsd('testdata/PAT02_idpExampleCom_sig.xml')
+    testXmlTools = TestXmlTools(schema_dir_abs)
+    testXmlTools.validate_xsd('testdata/PAT02_idpExampleCom_sig.xml')
     print('Schema validated')
+    testXmlTools.verify_xmlsig('testdata/PMPns05_idp5_signed_untrusted_signer.xml')
+    print('Signature validated')
