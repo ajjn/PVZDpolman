fae2675b6041b319fe7332d3dc71b2a6066df706
r2h2 <rainer@hoerbe.at>
2016-06-19 10:28:23 +0200

    make update optional

--- dependent_pkg/download.sh
+++ dependent_pkg/download.sh
@@ -2,12 +2,36 @@
 
 # Load dependent packages
 
+update_pkg="False"
+
+while getopts ":hnu" opt; do
+  case $opt in
+    n)
+      update_pkg="False"
+      ;;
+    u)
+      update_pkg="True"
+      ;;
+    *)
+      echo "usage: $0 [-n] [-u]
+   -n  do not update git repos in docker build context (default)
+   -u  update git repos in docker build context
+   "
+      exit 0
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
 get_or_update_repo() {
     if [ -e $repodir ] ; then
-        echo "pulling updates from $repodir"
-        cd $repodir && git pull && cd -    # already cloned
+        if [ "$update_pkg" == "True" ]; then
+            echo "updating $repodir"
+            cd $repodir && git pull && cd $OLDPWD
+        fi
     else
-        echo "cloning $repodir"
+        echo "cloning $repodir" \
         mkdir -p $repodir
         git clone $repourl $repodir        # first time
     fi
