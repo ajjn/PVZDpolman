b44600f74891f130a5f58c0b68dcbf98c05f69ef
r2h2 <rainer@hoerbe.at>
2016-07-07 14:07:09 +0200

    set pyjnius

--- PolicyManager/tests/setTestEnv.sh
+++ PolicyManager/tests/setTestEnv.sh
@@ -8,3 +8,6 @@ $PROJ_HOME/lib/unittests/hamcrest-core-1.3.jar:\
 $PROJ_HOME/lib/unittests/hamcrest-library-1.3.jar"
 
 export PYTHONPATH=$PYTHONPATH:$PROJ_HOME/PolicyManager/tests
+
+# remove following line to use javabridge
+export PYJNIUS_ACTIVATE=
