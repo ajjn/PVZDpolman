2e26602b3e6e5295141c924d4c35fa2ea3ef0517
r2h2 <rainer@hoerbe.at>
2016-10-16 14:12:03 +0200

    adde script to run all unit tests

---
+++ PolicyManager/tests/testAll.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# run all unit tests
+
+while getopts ":hs:" opt; do
+  case $opt in
+    s)
+      re='^(all|interactive|noninteractive)$'
+      if ! [[ $OPTARG =~ $re ]] ; then
+         echo "error: -s argument ($OPTARG) is not in 'all|interactive|noninteractive'"; exit 1
+      fi
+      subset=$OPTARG
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument"
+      exit 1
+      ;;
+    *)
+      echo "usage: $0 [-h] [-s all|interactive|noninteractive]"
+   -h  print this help text
+   -n  subset of tests
+      exit 0
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+SCRIPTDIR=$(cd $(dirname $BASH_SOURCE[0]) && pwd)
+source $SCRIPTDIR/setTestEnv.sh
+MOD_HOME=$(cd $(dirname $SCRIPTDIR) && pwd)
+PROJ_HOME=$(cd $(dirname $MOD_HOME) && pwd)
+export MOASPSS_LIB=$PROJ_HOME/lib/moa-spss-lib
+export POLMAN_TRUSTEDCERTS="$MODHOME/tests/testdata/trustedcerts.json"
+
+sum=0
+
+function interactiveTests {
+  $py3 $MOD_HOME/tests/testPMP_NoSig.py
+  sum=$(($sum+$?))
+  $py3 $MOD_HOME/tests/testPEP.py
+  sum=$(($sum+$?))
+}
+
+function noninteractiveTests {
+  $py3 $MOD_HOME/tests/testPMP_NoSig.py
+  sum=$(($sum+$?))
+  $py3 $MOD_HOME/tests/testPEP.py
+  sum=$(($sum+$?))
+}
+
+case $subset in
+  interactive)
+    interactiveTests
+    ;;
+  noninteractive)
+    noninteractiveTests
+    ;;
+  all)
+    noninteractiveTests
+    interactiveTests
+    ;;
+esac
+
+exit $sum
