c4733a9b27ccb4a6ee5dbc5d484f6b8f907fcb01
r2h2 <rainer@hoerbe.at>
2016-12-12 09:38:17 +0100

    fix test outside dev env

--- PolicyManager/tests/py2java_test.sh
+++ PolicyManager/tests/py2java_test.sh
@@ -7,6 +7,7 @@
 
 SCRIPTDIR=$(cd $(dirname $BASH_SOURCE[0]) && pwd)
 MOD_HOME=$(cd $(dirname $SCRIPTDIR) && pwd)
+PROJ_HOME=$(cd $(dirname $MOD_HOME) && pwd)
 
 source $MOD_HOME/bin/setEnv.sh  # get JAVA_HOME
 source $MOD_HOME/tests/setTestEnv.sh
@@ -15,11 +16,14 @@ echo "$0 (1): Calling xsdValidator/Jhades"
 $JAVA_HOME/bin/java at.wien.ma14.pvzd.validatexsd.cli.XSDValidatorCLI
 
 echo "$0 (2):Calling xsdValidator (from Java)"
-$JAVA_HOME/bin/java at.wien.ma14.pvzd.validatexsd.cli.XSDValidatorCLI testdata/PEP/01/idp4TestExampleOrg_idpXml.xml
+$JAVA_HOME/bin/java at.wien.ma14.pvzd.validatexsd.cli.XSDValidatorCLI \
+    testdata/PEP/01/idp5TestExampleOrg_idpXml.xml \
+    $PROJ_HOME/lib/SAML_MD_SCHEMA
 
 echo
 echo "$0 (3):Calling VerifySig (from Java)"
-$JAVA_HOME/bin/java at.wien.ma14.pvzd.verifysigapi.cli.VerifySigCLI testdata/PEP/03/policyDirectory_basic_MOA/policydir/pol_journal.xml
+$JAVA_HOME/bin/java at.wien.ma14.pvzd.verifysigapi.cli.VerifySigCLI \
+    testdata/PEP/03/policyDirectory_basic_MOA/policydir/pol_journal.xml
 
 echo
 echo "$0 (4):Calling xsdValidator + VerifySig (from Python)"
